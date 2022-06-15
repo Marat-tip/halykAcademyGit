df49fe3e73830caf3d6a97419309fa7de3fd75fc
Marat Kazgozhin <Marat.Kazgozhin@btsdigital.kz>
2022-06-15 20:50:52 +0600

    Dear to user

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world, Username");
+        System.out.println("Hello world,Dear Username");
     }
 }
